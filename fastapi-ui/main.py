d4a2c4582232a97061188a8c4886940746f85d98
Gabor Korodi <gkorodi@gmail.com>
2025-02-21 15:06:56 -0500

    Release UI

--- fastapi-ui/main.py
+++ fastapi-ui/main.py
@@ -1,18 +1,23 @@
-from fastapi import FastAPI, Request
+from typing import Annotated
+from fastapi import FastAPI, Request, Form, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
+import json
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse, name = "home")
+
+@app.get("/", response_class=HTMLResponse, name="home")
 async def home(request: Request):
     return templates.TemplateResponse(
         request=request, name="home.jinja", context={}
     )
+
 
 @app.get("/about", response_class=HTMLResponse)
 async def about(request: Request):
@@ -20,11 +25,13 @@ async def about(request: Request):
         request=request, name="about.jinja", context={}
     )
 
+
 @app.get("/contact", response_class=HTMLResponse)
 async def contact(request: Request):
     return templates.TemplateResponse(
         request=request, name="contact.jinja", context={}
     )
+
 
 @app.get("/posts/{id}", response_class=HTMLResponse)
 async def get_post_by_id(request: Request, id: str):
@@ -51,12 +58,23 @@ async def get_post_by_id(request: Request, id: str):
         request=request, name="post.jinja", context={"post_content": post_content}
     )
 
+
+def getAllPosts():
+    items = []
+    for file in os.listdir():
+        if file.endswith(".json"):
+            with open(file, 'r') as fp:
+                items.append(json.load(fp))
+    return items
+
+
 @app.get("/posts", response_class=HTMLResponse)
 async def get_all_posts(request: Request):
-    items = getAllItems()
+    posts = getAllPosts()
     return templates.TemplateResponse(
-        request=request, name="items.jinja", context={"items": items}
+        request=request, name="posts.jinja", context={"posts": posts}
     )
+
 
 @app.post("/posts", response_class=RedirectResponse)
 async def add_new_post(request: Request, id: Annotated[str, Form()], title: Annotated[str, Form()], copy_text: Annotated[str, Form()]):
@@ -64,7 +82,11 @@ async def add_new_post(request: Request, id: Annotated[str, Form()], title: Anno
     file_path = f"post_{id}.json"
 
     with open(file_path, 'w') as fp:
-        json.dump(sample, fp)
+        json.dump({
+            "id": id,
+            "title": title,
+            "content": copy_text
+        }, fp)
 
-
-    return RedirectResponse(url_for('get_all_posts'), status_code=status.HTTP_303_SEE_OTHER) 
+    # https://stackoverflow.com/questions/63682956/fastapi-retrieve-url-from-view-name-route-name
+    return RedirectResponse(app.url_path_for('get_all_posts'), status_code=status.HTTP_303_SEE_OTHER)
